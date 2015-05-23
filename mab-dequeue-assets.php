ffd5ac18496a22ff55984668e5a65ff0a4b3fd63
Ryann Micua <ryannmicua@pogidude.com>
2015-05-23 21:27:45 +0800

    update description

--- mab-dequeue-assets.php
+++ mab-dequeue-assets.php
@@ -3,7 +3,7 @@
 /*
 Plugin Name: MAB Dequeue CSS/JS
 Plugin URI: http://magicactionbox.com
-Description: Prevent CSS and JS files from being loaded
+Description: Prevent CSS and JS files from being loaded by dequeueing the file through wp_dequeue_script and wp_dequeue_css
 Version: 1.0
 Author: pogidude
 Author URI: http://pogidude.com
