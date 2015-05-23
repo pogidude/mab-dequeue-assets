b8bd6360cf7d581df43b539ff23fdc3813fc6d51
Ryann Micua <ryannmicua@pogidude.com>
2015-05-23 21:26:18 +0800

    initial commit

---
+++ mab-dequeue-assets.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+Plugin Name: MAB Dequeue CSS/JS
+Plugin URI: http://magicactionbox.com
+Description: Prevent CSS and JS files from being loaded
+Version: 1.0
+Author: pogidude
+Author URI: http://pogidude.com
+License: A "Slug" license name e.g. GPL2
+*/
+
+add_action('wp_enqueue_scripts', 'mabdequeue_assets', 101);
+function mabdequeue_assets(){
+	// magic action box
+	wp_dequeue_script('mab-ajax-form');
+
+	// magic action popup
+	wp_dequeue_script('map-button-popup');
+}
